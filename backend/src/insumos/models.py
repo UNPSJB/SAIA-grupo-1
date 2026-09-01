e80a85df99cbf49deab299b9d0f7b74e154548c5
Simon Escobar <simonesc222@gmail.com>
2026-09-01 18:17:02 -0300

    Backend : modelo de insumos

---
+++ backend/src/insumos/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from enum import auto, StrEnum
+from src.models import ModeloBase
+
+class UnidadMedida(StrEnum):
+    KILOGRAMOS = auto()
+    GRAMOS = auto()
+    LITROS = auto()
+    MILILITROS = auto()
+
+
+
+class Insumo(ModeloBase):
+    __tablename__ = "insumos"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    nombre: Mapped[str] = mapped_column(index=True)
+    tipo: Mapped[UnidadMedida] = mapped_column()  
