5c21c67c34d46b06312e53911ba5d0b6ba30d99a
Thiago <thiagobrunt@gmail.com>
2026-09-07 17:08:33 -0300

    Modificaciones de archivos(Cambie nombre nomas)

--- backend/tests/database.py
+++ backend/tests/database.py
@@ -6,11 +6,8 @@ from src.main import app
 from src.database import get_db
 from src.config import settings
 from src.models import ModeloBase
-from src.personas.services import crear_persona
-from src.mascotas.services import crear_mascota
-from src.personas.schemas import PersonaCreate
-from src.mascotas.schemas import MascotaCreate
-from src.mascotas.models import TipoMascota
+from src.personal.services import crear_personal
+from src.personal.schemas import PersonalCreate
 
 
 # creamos una db para testing
@@ -47,21 +44,15 @@ def session() -> Generator[Session, None, None]:
 
     # aqui podemos crear instancias de objetos para hacer tests
     # haciendo uso de las funciones "create_<clase>" de services y los schemas <Clase>Create.
-    persona_1 = crear_persona(db, PersonaCreate(nombre="Juan", email="juan.perez@gmail.com"))
-    persona_2 = crear_persona(
-        db, PersonaCreate(nombre="Ana", email="ana.dominguez@gmail.com")
+    persona_1 = crear_personal(db, PersonalCreate(nombre="Juan", email="juan.perez@gmail.com"))
+    persona_2 = crear_personal(
+        db, PersonalCreate(nombre="Ana", email="ana.dominguez@gmail.com")
     )
-    mascota_1 = crear_mascota(db, MascotaCreate(nombre="Lola", tipo=TipoMascota.GATO, tutor_id=persona_1.id))
-    mascota_2 = crear_mascota(db, MascotaCreate(nombre="Felipe", tipo=TipoMascota.PERRO, tutor_id=persona_1.id))
-    mascota_3 = crear_mascota(db, MascotaCreate(nombre="Coco", tipo=TipoMascota.COBAYO, tutor_id=persona_2.id))
 
     db.add_all(
         [
             persona_1,
-            persona_2,
-            mascota_1,
-            mascota_2,
-            mascota_3
+            persona_2
         ]
     )
     db.commit()
