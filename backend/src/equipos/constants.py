f4d5ffeb0b6b35bb2e86eb7d41d0ae3b928f6023
kevin_1 <kevinaspirios@gmail.com>
2026-09-10 22:18:31 -0300

    corregi unos mensajes de error

--- backend/src/equipos/constants.py
+++ backend/src/equipos/constants.py
@@ -4,4 +4,4 @@ class ErrorCode:
     EQUIPO_NO_TIENE_NINGUN_PLAN = "El equipo no tiene ningún plan asignado."
     NOMBRE_CON_NUMEROS = "El nombre no puede contener numeros"
     UBICACION_CON_NUMERO=" La Ubicacion no puede contener numeros"
-    CARACTERES_MAYOR_A_CUATRO=" La cantidad de caracteres debe ser mayor o igual a 4"
+    CARACTERES_MAYOR_A_CUATRO=" La cantidad de caracteres nombre y ubicacion debe ser mayor o igual a 4"
