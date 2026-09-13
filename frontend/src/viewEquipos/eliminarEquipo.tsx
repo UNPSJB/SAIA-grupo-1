d8d28bb43815b313a9f58ef411c26889cc9ba160
kevin_1 <kevinaspirios@gmail.com>
2026-09-13 20:46:46 -0300

    agregue el cartel o mensaje para peregutar si esta seguro de la eliminaacion

--- frontend/src/viewEquipos/eliminarEquipo.tsx
+++ frontend/src/viewEquipos/eliminarEquipo.tsx
@@ -15,6 +15,7 @@ export default function EliminarEquipo({equipoID,onCancel,onSucces}:EliminarEqui
     const[loading, setLoading]= useState(true);
 
     const dialog= useRef<HTMLDialogElement>(null);
+    const dialogSeguro= useRef<HTMLDialogElement>(null);
 
 const handleEliminar = async (id?: number) => {
         if (!id) return;
@@ -134,7 +135,7 @@ const handleEliminar = async (id?: number) => {
                 
                 )}
 
-                <div className="form-acciones"> <button className="btn-eliminar" title="Eliminar" onClick={() => handleEliminar(equipoID ?? undefined)}>
+                <div className="form-acciones"> <button className="btn-eliminar" title="Eliminar" onClick={() => dialogSeguro.current?.showModal()}>
                                         Eliminar
                         </button>
                         
@@ -142,6 +143,12 @@ const handleEliminar = async (id?: number) => {
                     Cancelar
                 </button>
                 </div>
+
+                <dialog ref={dialogSeguro} className="seguro">
+                    <h2>Esta seguro de eliminar este equipo?</h2>
+                    <button type="button" className="btn-eliminar" onClick={() => handleEliminar(equipoID ?? undefined)}>Eliminar</button>
+                    <button type="button" className="btn-cancelar" onClick={() => {dialogSeguro.current?.close(); onCancel}}>Cancelar</button>
+                </dialog>
 
                 <dialog ref={dialog} className="eliminado-exito">
                     <h2>Eliminacion Exitosa</h2>
